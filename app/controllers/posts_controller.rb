3fcaa591b3f6af3584ef79bb2ddb0c2ab3c83886
Chudkko <chudkko@gmail.com>
2017-06-29 19:29:07 +0800

    add new post form and logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save    
+      redirect_to @post
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
